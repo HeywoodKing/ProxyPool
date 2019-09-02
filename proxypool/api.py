5914a71bd7c4dd7fc81f3182decede7b173173a9
flackmaster <opencoding@hotmail.com>
2019-09-02 13:54:31 +0800

    修改setting.py参数，增加api

--- proxypool/api.py
+++ proxypool/api.py
@@ -7,6 +7,22 @@ __all__ = ['app']
 app = Flask(__name__)
 
 
+# 打印获取到的代理
+# 无固定参数带返回值的装饰器
+def outter(func):
+    def inner(*args, **kwargs):
+        import time
+        start_time = time.time()
+        res = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = (end_time - start_time) * 1000
+        print("time is %d ms" % execution_time)
+        print('\b 取到随机代理：{}'.format(res), end='', flush=True)
+        return res
+
+    return inner
+
+
 def get_conn():
     if not hasattr(g, 'redis'):
         g.redis = RedisClient()
@@ -19,7 +35,6 @@ def index():
 
 
 @app.route('/proxy')
-@app.route('/ip')
 @app.route('/get')
 @app.route('/random')
 def get_proxy():
@@ -39,6 +54,42 @@ def get_counts():
     """
     conn = get_conn()
     return str(conn.count())
+
+
+@app.route('/pool')
+def get_proxy_all():
+    """
+    获取代理池中所有的ip列表
+    :return:
+    """
+    conn = get_conn()
+    return list(conn.all())
+
+
+@outter
+@app.route('/proxy/<port>')
+def get_proxy_by_port(port):
+    """
+    获取指定端口的一个ip
+    :param port:
+    :return:
+    """
+    conn = get_conn()
+    for address in conn.all():
+        try:
+            ip = address.split(':')[-1]
+            if ip == port:
+                return ip
+        except Exception as ex:
+            print('get_ip_by_port:{}'.format(ex))
+
+
+@outter
+@app.route('/proxy/<start>/<stop>')
+def get_proxy_range(start, stop):
+    print('start', start)
+    print('\b stop:', stop)
+    return start + '/' + stop
 
 
 if __name__ == '__main__':
