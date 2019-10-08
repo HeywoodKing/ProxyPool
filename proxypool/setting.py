6a6ea794b8ce9990699e21fc0ac8bb1b27a0851b
flack.chen <flack.chen@icmofang.com>
2019-10-08 21:32:25 +0800

    优化bug

--- proxypool/setting.py
+++ proxypool/setting.py
@@ -1,13 +1,12 @@
-# Redis数据库地址
-REDIS_HOST = '192.168.1.141'
+# # Redis数据库地址
+# REDIS_HOST = '192.168.1.141'
+# # Redis端口
+# REDIS_PORT = 6379
+# # Redis密码，如无填None
+# REDIS_PASSWORD = None
+# REDIS_KEY = 'proxies'
 
-# Redis端口
-REDIS_PORT = 6379
 
-# Redis密码，如无填None
-REDIS_PASSWORD = None
-
-REDIS_KEY = 'proxies'
 
 # 代理分数
 MAX_SCORE = 100
